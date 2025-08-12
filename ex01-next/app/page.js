8f7914b48c416ea4a237ea068c5237700c0b7657
Rsfrancis <arturfdamas2@gmail.com>
2025-08-12 00:31:42 +0000

    Hello world

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default fuction Home() {
-  return <div> Olá turma! </div>
+export default function Home() {
+  return <div> Olá turma! </div>;
 }
